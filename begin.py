6304470fe6fe87a3a2a7df6d493bfa8eadc3d502
MercyL1n <18506000485@163.com>
2020-07-03 15:35:55 +0800

    修复了插入数据库时遇到语法错误，键值为空等bug

--- begin.py
+++ begin.py
@@ -18,27 +18,26 @@ crawl_list = {
     'https://news.sina.com.cn/': 'SinaHotspot',
     'http://www.xinhuanet.com/': 'XinhuaHotspot',
     'http://news.baidu.com/': 'BaiduHotspot',
-    'http://huanqiu.com': 'HuanqiuHotspot'
 }
-
-# if __name__ == '__main__':
-#     # 获取数据库
-#     db = MysqlOperation()
-#     # 获取数据库中储存的网站列表
-#     website_list = db.get_website_list()
-#     # 断开数据库
-#     db.dis_connect()
-#     # scrapy 通过 CrawlerProcess 来同时运行多个爬虫
-#     process = CrawlerProcess(get_project_settings())
-#     # 将可以爬取的站点加入爬取列表
-#     for website in website_list:
-#         if website[1] in crawl_list.keys():
-#             process.crawl(crawl_list[website[1]])
-#             print(str(website[2]) + "加入爬取列表")
-#         else:
-#             print("暂不支持", end="")
-#             print(website[2] if website[2] is not None else website[1])
-#     print("-----------------start-------------------")
-#     # 开始爬取
-#     process.start()
-cmdline.execute("scrapy crawl HuanqiuHotspot".split())
+#     'http://huanqiu.com': 'HuanqiuHotspot'
+if __name__ == '__main__':
+    # 获取数据库
+    db = MysqlOperation()
+    # 获取数据库中储存的网站列表
+    website_list = db.get_website_list()
+    # 断开数据库
+    db.dis_connect()
+    # scrapy 通过 CrawlerProcess 来同时运行多个爬虫
+    process = CrawlerProcess(get_project_settings())
+    # 将可以爬取的站点加入爬取列表
+    for website in website_list:
+        if website[1] in crawl_list.keys():
+            process.crawl(crawl_list[website[1]])
+            print(str(website[2]) + "加入爬取列表")
+        else:
+            print("暂不支持", end="")
+            print(website[2] if website[2] is not None else website[1])
+    print("-----------------start-------------------")
+    # 开始爬取
+    process.start()
+# cmdline.execute("scrapy crawl HuanqiuHotspot".split())
